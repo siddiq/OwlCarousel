f82bbd8f9ce04df6a7a7a90f04c58f9cea7f872a
siddiq <siddiq@gmail.com>
2014-10-23 16:26:35 +0400

    Update owl.carousel.js
    A better way to detect 3d support.

--- owl-carousel/owl.carousel.js
+++ owl-carousel/owl.carousel.js
@@ -787,6 +787,37 @@ if (typeof Object.create !== "function") {
             });
         },
 
+        /**
+         * A better way to detect 3d support.
+         * https://gist.github.com/lorenzopolidori/3794226
+         * https://stackoverflow.com/questions/5661671/detecting-transform-translate3d-support
+         */
+        has3d : function () {
+            var el = document.createElement('p'),
+                has3d,
+                transforms = {
+                    'webkitTransform':'-webkit-transform',
+                    'OTransform':'-o-transform',
+                    'msTransform':'-ms-transform',
+                    'MozTransform':'-moz-transform',
+                    'transform':'transform'
+                };
+
+            // Add it to the body to get the computed style
+            document.body.insertBefore(el, null);
+
+            for (var t in transforms) {
+                if (el.style[t] !== undefined) {
+                    el.style[t] = 'translate3d(1px,1px,1px)';
+                    has3d = window.getComputedStyle(el).getPropertyValue(transforms[t]);
+                }
+            }
+
+            document.body.removeChild(el);
+
+            return (has3d !== undefined && has3d.length > 0 && has3d !== "none");
+        },
+
         checkBrowser : function () {
             var base = this,
                 translate3D = "translate3d(0px, 0px, 0px)",
@@ -808,7 +839,7 @@ if (typeof Object.create !== "function") {
             isTouch = "ontouchstart" in window || window.navigator.msMaxTouchPoints;
 
             base.browser = {
-                "support3d" : support3d,
+                "support3d" : this.has3d(),
                 "isTouch" : isTouch
             };
         },
